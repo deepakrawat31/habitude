d61d59b553217e233efbc3e328fea5252ad97013
Deepak Rawat <deepakrawat.mb@gmail.com>
2025-03-27 23:36:04 +0530

    Initial commit

---
+++ src/components/Sidebar.svelte
@@ -0,0 +1,1 @@
+<aside class="w-full max-w-xs rounded bg-neutral-900"></aside>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Sidebar from '../components/Sidebar.svelte';
+</script>
+
+<svelte:head>
+	<title>Habitude</title>
+</svelte:head>
+
+<div class="flex min-h-dvh gap-4 p-4">
+	<Sidebar />
+</div>
