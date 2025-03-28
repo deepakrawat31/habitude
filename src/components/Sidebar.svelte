757b3bfd7f3febfba2f372413d702c62e0d5ea15
Deepak Rawat <deepakrawat.mb@gmail.com>
2025-03-28 23:28:29 +0530

    sidebar update

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -1,1 +1,17 @@
-<aside class="w-full max-w-xs rounded bg-neutral-900"></aside>
+<script>
+	import { Plus } from '@lucide/svelte';
+</script>
+
+<aside class="flex w-full max-w-xs flex-col justify-between gap-8 rounded bg-neutral-900 p-4">
+	<ul class="flex items-center justify-between gap-4">
+		<li><a href="/"><img src="/favicon.ico" alt="icon" class="size-8" /></a></li>
+		<li>
+			<button
+				class="inline-flex cursor-pointer rounded-full bg-neutral-800 p-2 ring-1 ring-neutral-100/20"
+			>
+				<Plus class="size-4" />
+			</button>
+		</li>
+	</ul>
+	<div class=""></div>
+</aside>
