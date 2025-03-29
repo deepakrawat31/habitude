d5cc2714c4080a8bcf828e6a2b3f5dfb021ee112
Deepak Rawat <deepakrawat.mb@gmail.com>
2025-03-29 23:58:33 +0530

    sidebar update

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -2,16 +2,16 @@
 	import { Plus } from '@lucide/svelte';
 </script>
 
-<aside class="flex w-full max-w-xs flex-col justify-between gap-8 rounded bg-neutral-900 p-4">
+<aside class="flex w-full max-w-xs flex-col gap-8 rounded bg-neutral-900 p-4">
 	<ul class="flex items-center justify-between gap-4">
 		<li><a href="/"><img src="/favicon.ico" alt="icon" class="size-8" /></a></li>
 		<li>
 			<button
-				class="inline-flex cursor-pointer rounded-full bg-neutral-800 p-2 ring-1 ring-neutral-100/20"
+				class="inline-flex cursor-pointer rounded-full bg-emerald-800 p-2 text-emerald-400 transition-transform duration-300 hover:scale-90"
 			>
 				<Plus class="size-4" />
 			</button>
 		</li>
 	</ul>
-	<div class=""></div>
+	<div class="flex-1"></div>
 </aside>
