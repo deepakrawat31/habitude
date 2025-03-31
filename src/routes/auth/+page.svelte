f8fedb590de13329e72859eabead262a5784a692
Deepak Rawat <deepakrawat.mb@gmail.com>
2025-03-31 22:21:54 +0530

    new font

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,7 +1,27 @@
 <section class="relative flex min-h-dvh p-4">
-	<form action="" class="flex w-full max-w-md flex-col gap-8 rounded-md bg-neutral-900 p-4">
-		<span class="text-2xl font-bold uppercase">habitude</span>
-		<p></p>
-	</form>
+	<div class="flex w-full max-w-md flex-col justify-center gap-6 rounded-md bg-neutral-900 p-4">
+		<span class="text-4xl font-bold">Habitude</span>
+		<p class="text-neutral-100/40">Start building, block by block.</p>
+		<form action="" class="flex flex-col gap-6">
+			<label for="mail" class="flex flex-col gap-4">
+				<span>Email</span>
+				<input
+					type="email"
+					name="mail"
+					id="mail"
+					class="rounded bg-neutral-800 p-2 ring-1 ring-neutral-100/20"
+				/>
+			</label>
+			<label for="passw" class="flex flex-col gap-4">
+				<span>Password</span>
+				<input
+					type="password"
+					name="passw"
+					id="passw"
+					class="rounded bg-neutral-800 p-2 ring-1 ring-neutral-100/20"
+				/>
+			</label>
+		</form>
+	</div>
 	<div class="absolute inset-0 -z-30"></div>
 </section>
