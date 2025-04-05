0b034b8fcf5c0c737a12fbce6e506ae09d271c8f
Deepak Rawat <deepakrawat.mb@gmail.com>
2025-04-05 23:03:02 +0530

    small update

--- src/lib/server/schema.ts
+++ src/lib/server/schema.ts
@@ -1,6 +1,6 @@
 import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
-import { db } from '../server/db/drizzle'; // your drizzle instance
+import { db } from './db/drizzle'; // your drizzle instance
 
 export const auth = betterAuth({
 	database: drizzleAdapter(db, {
