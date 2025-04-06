7c25b09f05d04242aaba6d61690a943d287f110d
Deepak Rawat <deepakrawat.mb@gmail.com>
2025-04-06 18:50:34 +0530

    schema added

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,8 +4,8 @@ import { defineConfig } from 'drizzle-kit';
 config({ path: '.env' });
 
 export default defineConfig({
-	schema: './src/lib/server/schema.ts',
-	out: './migrations',
+	schema: './src/lib/db/schema.ts',
+	out: '.src/lib/db/migrations',
 	dialect: 'postgresql',
 	dbCredentials: {
 		url: process.env.DATABASE_URL!
