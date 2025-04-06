c28e7a4986c8027d6a1c2e12f09bdfb0c46fbf23
Deepak Rawat <deepakrawat.mb@gmail.com>
2025-04-07 00:00:59 +0530

    changed to next.js

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+   return (
+      <main className="flex p-4 gap-4 min-h-dvh">
+         <aside className="max-w-xs w-full rounded-md bg-neutral-900 p-4"></aside>
+         <div className=""></div>
+      </main>
+   );
+}
